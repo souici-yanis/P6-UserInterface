578aa96787406e4b81a2fb06b6f8f7f36fa6dcd5
souici-yanis <souici.yanis@gmail.com>
2020-03-11 00:15:00 +0100

    books card display

--- bookScript.js
+++ bookScript.js
@@ -1,19 +1,21 @@
 function init(){
+    debugger;
     var userDisplay = document.getElementById("userDisplay");
     if(!userDisplay){
         var div = document.createElement("div");
         div.id ="userDisplay";
         div.innerHTML = '<button id="button">Ajouter un livre</button>';
+
+        var buttonPlace = document.querySelector("#myBooks hr");
+        var buttonParent = document.getElementById("myBooks");
+        
+        buttonParent.insertBefore(div, buttonPlace); 
     } else {
         userDisplay.innerHTML = '<button id="button">Ajouter un livre</button>';
-        userDisplay.classList.remove("searchClass");
         userDisplay.id ="userDisplay";
+        userDisplay.classList.remove("searchClass");
     }
-    
-    var buttonPlace = document.querySelector("#myBooks hr");
-    var buttonParent = document.getElementById("myBooks");
-    
-    buttonParent.insertBefore(div, buttonPlace);           
+
     document.getElementById("button").addEventListener("click", addBook);
 }
 
@@ -32,10 +34,17 @@ function addBook(){
 }
 
 function cancelSearch(){
-    init()
+    init();
+    clearBooks();
+}
+
+function clearBooks(){
+    var content = document.getElementById('content');
+    content.innerHTML = '<h2>Ma poch\'liste</h2>';
 }
 
 function searchBook(){
+    clearBooks();
     var xhr = new XMLHttpRequest();
     var titleBook = document.getElementById("titleBook").value;
     var auteur = document.getElementById("auteur").value;
@@ -47,11 +56,32 @@ function searchBook(){
 
         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) { // Si le fichier est chargé sans erreur
             var result = JSON.parse(xhr.responseText);
-            console.log(result);
-            console.log('Livre 1 ' + result.items[0].volumeInfo.title + ' écrit par ' + result.items[0].volumeInfo.authors);
-            console.log('Livre 2 ' + result.items[1].volumeInfo.title + ' écrit par ' + result.items[1].volumeInfo.authors);
+            var content = document.getElementById('content');
+            var divContent = document.createElement('div');
+            divContent.id= "listBook"
+            content.appendChild(divContent);
+            for (var i=0; i<result.items.length; i++){ 
+                console.log(result.items[i]);        
+                createItem(result.items[i]);
+            }
         }
     });  
+}
+
+function createItem(book) {
+    var content = document.getElementById('listBook');
+    var divContent = document.createElement('div');
+    divContent.innerHTML += '<h3> Titre : ' + book.volumeInfo.title + '</h3>';
+    divContent.innerHTML += '<p> Id : ' + book.id + '</p>';
+    divContent.innerHTML += '<p> Auteur : ' + book.volumeInfo.authors[0] + '</p>';
+    divContent.innerHTML += '<p> Description : ' + book.volumeInfo.description.substring(0,200) + '...</p>';
+    if(book.volumeInfo.imageLinks.smallThumbnail){
+        divContent.innerHTML += '<div class="imageBook"><img src="' + book.volumeInfo.imageLinks.smallThumbnail + '"></div>';
+    } else {
+        //divContent.innerHTML += '<img src="unavailable.png">';
+    }
+    divContent.classList.add("bookCard");
+    content.appendChild(divContent);
 }
 
 window.addEventListener('DOMContentLoaded', init);

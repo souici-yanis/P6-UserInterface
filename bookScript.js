a2fc54fa3c12a9a64c437c39a5c06a2c127edb79
souiciyanis <souici.yanis@gmail.com>
2020-03-11 16:53:12 +0100

    all functionalities ok

--- bookScript.js
+++ bookScript.js
@@ -1,5 +1,4 @@
 function init(){
-    debugger;
     var userDisplay = document.getElementById("userDisplay");
     if(!userDisplay){
         var div = document.createElement("div");
@@ -15,11 +14,12 @@ function init(){
         userDisplay.id ="userDisplay";
         userDisplay.classList.remove("searchClass");
     }
-
+    getBook(); // TODO get books via map to create
     document.getElementById("button").addEventListener("click", addBook);
 }
 
 function addBook(){
+    setTitle('Recherche');
     var userDisplay = document.getElementById("userDisplay");
     userDisplay.innerHTML = '<label for="name">Titre du livre</label>';
     userDisplay.innerHTML += '<input type="text" id="titleBook" name="name" required>';
@@ -30,24 +30,95 @@ function addBook(){
     userDisplay.classList.add("searchClass");
     document.getElementById("search").addEventListener("click", searchBook);
     document.getElementById("cancel").addEventListener("click", cancelSearch);
-
+    document.getElementById("titleBook").addEventListener("keyup", checkFieldEmpty);
+    document.getElementById("auteur").addEventListener("keyup", checkFieldEmpty);
 }
 
 function cancelSearch(){
     init();
-    clearBooks();
+    setTitle('Ma Poch\'List');
+    clearListBook()
+    getBook();
 }
 
-function clearBooks(){
-    var content = document.getElementById('content');
-    content.innerHTML = '<h2>Ma poch\'liste</h2>';
+function checkFieldEmpty(ev){
+    if(ev.target.validity.valueMissing){
+        createErrorInput(ev.target.attributes[1].value, 'Merci de renseigner le champ.');
+        return false;
+    } else {
+        removeErrorInput(ev.target.attributes[1].value);
+        return true;
+    }
+}
+
+function clearListBook() {
+    var listBook = document.getElementById("listBook");
+    if (listBook){
+        listBook.innerHTML = '';
+    }
+}
+
+function getBook() {
+    var idBook = sessionStorage.getItem('book');
+    console.log('getBook : ', idBook);
+    if(!idBook){
+        return;
+    }
+    for(var i=0; i<idBook.length; i++){
+        var bookItemId = idBook.split(',')[i];
+        var bookItem = maptest.get(bookItemId);
+        if (bookItem) {
+            createItem(bookItem, true);
+        }
+    }
+}
+
+function setTitle(title) {
+    var buttonPlace = document.querySelector("#content h2");
+    buttonPlace.innerHTML = '<h2>' +title + '</h2>'
+}
+
+function createErrorInput(id, errorMessage){
+    var idError = id + 'error';
+    if(document.getElementById(idError)){
+        return;
+    } 
+    var inputError = document.createElement("div");
+    inputError.id = id + 'error';
+    inputError.innerHTML = errorMessage;
+    inputError.setAttribute("style", "color: red;");
+    
+    var div = document.getElementById(id);
+    div.insertAdjacentElement('afterend', inputError);    
+    div.classList.add("inputError");
+}
+
+function removeErrorInput(id){
+    var idError = id + 'error';
+    var messageError = document.getElementById(idError);
+    var inputError = document.getElementById(id);
+    
+    inputError.classList.remove("inputError");
+    if(messageError){
+        messageError.remove();
+    }
 }
 
 function searchBook(){
-    clearBooks();
+    setTitle('Recherche');
+    clearListBook();
     var xhr = new XMLHttpRequest();
     var titleBook = document.getElementById("titleBook").value;
     var auteur = document.getElementById("auteur").value;
+    if (titleBook == ''){
+        createErrorInput('titleBook', 'Merci de renseigner le champ.');
+        return;
+    }
+    if (auteur == ''){
+        createErrorInput('auteur', 'Merci de renseigner le champ.');
+        return;
+    } 
+    
     var request = 'https://www.googleapis.com/books/v1/volumes?q=' + titleBook + '+inauthor:'+ auteur;
     xhr.open('GET', request);
     xhr.send();
@@ -56,32 +127,91 @@ function searchBook(){
 
         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) { // Si le fichier est chargé sans erreur
             var result = JSON.parse(xhr.responseText);
-            var content = document.getElementById('content');
-            var divContent = document.createElement('div');
-            divContent.id= "listBook"
-            content.appendChild(divContent);
-            for (var i=0; i<result.items.length; i++){ 
-                console.log(result.items[i]);        
-                createItem(result.items[i]);
+            if(result.totalItems){
+                setTitle('Résultat de la recherche');
+                var content = document.getElementById('content');
+                var divContent = document.createElement('div');
+                divContent.id= "listBook";
+                content.appendChild(divContent);
+                
+                for (var i=0; i<result.items.length; i++){        
+                    createItem(result.items[i], false);
+                }
+            } else { 
+                setTitle('Aucun livre n\'a été trouvé.');
             }
         }
     });  
 }
+var maptest = new Map();
 
-function createItem(book) {
+function createItem(book, isPochList) {
+    if(book && maptest.get(book.id) == null){
+        maptest.set(book.id, book);
+    }
+
     var content = document.getElementById('listBook');
     var divContent = document.createElement('div');
     divContent.innerHTML += '<h3> Titre : ' + book.volumeInfo.title + '</h3>';
+    if(isPochList){
+        divContent.innerHTML += '<div data-bookid="' + book.id + '" id="' + book.id + '" class="oneBookMark delete"></div>';
+    } else {
+        divContent.innerHTML += '<div data-bookid="' + book.id + '" id="' + book.id + '" class="oneBookMark bookMark"></div>';
+    }
+
     divContent.innerHTML += '<p> Id : ' + book.id + '</p>';
     divContent.innerHTML += '<p> Auteur : ' + book.volumeInfo.authors[0] + '</p>';
-    divContent.innerHTML += '<p> Description : ' + book.volumeInfo.description.substring(0,200) + '...</p>';
-    if(book.volumeInfo.imageLinks.smallThumbnail){
-        divContent.innerHTML += '<div class="imageBook"><img src="' + book.volumeInfo.imageLinks.smallThumbnail + '"></div>';
+    if(book.volumeInfo.description){
+        divContent.innerHTML += '<p> Description : ' + book.volumeInfo.description.substring(0,200) + '...</p>';
     } else {
-        //divContent.innerHTML += '<img src="unavailable.png">';
+        divContent.innerHTML += '<p> Description : Information manquante</p>';
+    }
+
+    if(book.volumeInfo.imageLinks){
+        divContent.innerHTML += '<div class="imageBook"><img style="width:100px;" src="' + book.volumeInfo.imageLinks.smallThumbnail + '"></div>';
+    } else {
+        divContent.innerHTML += '<img src="unavailable.png" style="width:150px; display: block; margin-left: auto; margin-right: auto">';
     }
     divContent.classList.add("bookCard");
     content.appendChild(divContent);
+    
+    if(isPochList){
+        document.getElementById(book.id).addEventListener("click", deleteBook);
+    } else {
+        document.getElementById(book.id).addEventListener("click", saveBook);
+    }
+}
+
+function saveBook(ev){
+    var clickedBookId = ev.target.dataset.bookid;
+    var choosenBookId = [];
+    if(!sessionStorage.getItem('book')){
+        choosenBookId.push(clickedBookId);
+        sessionStorage.setItem('book', choosenBookId);
+    } else {
+        var books = sessionStorage.getItem('book');
+        if(books.includes(clickedBookId)){
+            alert('Vous ne pouvez ajouter deux fois le même livre.');
+        } else {
+            var divBook = document.getElementById(clickedBookId);
+            divBook.classList.remove("bookMark");
+            divBook.classList.add("bookClicked");
+            
+            choosenBookId.push(books);
+            choosenBookId.push(clickedBookId);
+            sessionStorage.setItem('book', choosenBookId);
+        }
+    }
+}
+
+function deleteBook(ev){
+    var allBooks = sessionStorage.getItem('book').split(',');
+    var clickedBookId = ev.target.dataset.bookid;
+    var indexBook = allBooks.indexOf(clickedBookId);
+    allBooks.splice(indexBook, 1);
+    sessionStorage.setItem('book', allBooks);
+    clearListBook();
+    getBook();
 }
 
 window.addEventListener('DOMContentLoaded', init);
